363e0f9251d0d400328917a437a10f5f05b29545
larsjoR <larsjo29@gmail.com>
2016-10-05 19:22:21 +0200

    Cleaned code and doc'ed

--- WebApp-B2C-DotNet/Controllers/HomeController.cs
+++ WebApp-B2C-DotNet/Controllers/HomeController.cs
@@ -38,6 +38,7 @@ namespace WebApp_OpenIDConnect_DotNet_B2C.Controllers
             return View("Error");
         }
 
+        // Action to call Userinfo endpoint 
         public ActionResult Userinfo()
         {
             var identity = (ClaimsIdentity)ClaimsPrincipal.Current.Identity;
@@ -64,63 +65,55 @@ namespace WebApp_OpenIDConnect_DotNet_B2C.Controllers
         }
     }
 
+    // Simple helper class to call userinfo endpoint. 
+    // Class should be moved in its entirety out of HomeController scope.
     public class Helper
     {
+        // Should obviously be moved to app.config
         private string clientId = "DotNetClient";
         private string clientSecret = "1234";
         private string redirectUri = "https://localhost:44320/";
         private string OIDC_baseUrl = "https://preprod.bankidapis.no/oidc/oauth/";
-       
 
-        
+        public Helper() { }
 
-        public Helper() {
-            
-        }
-
+        /// <summary>
+        /// Simple method to call the OIDC userinfo enpoint
+        /// </summary>
+        /// <param name="bidCode">The bid_code parameter to be passed along in GetToken()</param>
+        /// <returns>A task containing JSON objects</returns>
         public async Task<JObject> CallUserinfoEndpoint(string bidCode)
         {
-            // Verified until this point
+            // Get token
             string token = GetToken(bidCode).Result.ToString();
-
-            
+  
             HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Post, OIDC_baseUrl + "userinfo");
-
-            // msg.Headers.Authorization = new AuthenticationHeaderValue("Bearer",Convert.ToBase64String(Encoding.UTF8.GetBytes(token)));
-
+            
             msg.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
+            // Send message. Use ConfigureAwait(false) to circumvent deadlock
             var response = await (new HttpClient()).SendAsync(msg,HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
 
-            //throw new Exception("Token: "+ token + "\nResponse: " + response);
-
             JObject tokenResponse = JObject.Parse(await response.Content.ReadAsStringAsync());
-
 
             return tokenResponse;
         }
 
+        /// <summary>
+        /// Simple method to get an access token from the OIDC endpoint
+        /// </summary>
+        /// <param name="bidCode">The bid_code parameter to be passed on in the web-request</param>
+        /// <returns>The access token, stringified</returns>
         public async Task<string> GetToken(string bidCode)
         {
-
-            
             HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Post, OIDC_baseUrl + "token");
-
-
             msg.Headers.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes(clientId + ":" + clientSecret)));
-          
-
             msg.Content = new StringContent(" code=" + bidCode + "&redirect_uri=" + redirectUri + "&grant_type=authorization_code",
                                             Encoding.UTF8,
                                             "application/x-www-form-urlencoded");
 
-            //var body = await (msg.Content.ReadAsStringAsync());
-
-           // throw new Exception("Message: \n" + msg + "\n: Content: \n" + body);
-
+            // Send message. Use ConfigureAwait(false) to circumvent deadlock
             var response = await (new HttpClient()).SendAsync(msg,HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
-
-            // throw new Exception("Response: \n" + response);
 
             JObject tokenResponse = JObject.Parse(await response.Content.ReadAsStringAsync());
 
